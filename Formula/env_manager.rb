0ec9cf6d4b1fae71757974971ea178f564e22834
stanyzra <stanyhelberth@gmail.com>
2024-03-28 19:46:34 -0300

    fix: include resources for boto3 in formulae

--- Formula/env_manager.rb
+++ Formula/env_manager.rb
@@ -3,8 +3,8 @@ class EnvManager < Formula
 
     desc "A environment variable manager for AWS Amplify and ECS, initially designed for Collection"
     homepage "https://github.com/stanyzra/homebrew-aws-env-manager"
-    url "https://github.com/stanyzra/homebrew-aws-env-manager/archive/refs/tags/v1.0.1.tar.gz"
-    sha256 "fe4249c6db12dfd88e70ebc7427e708355acdcb7fcabca17c3e4717e64546f76"
+    url "https://github.com/stanyzra/homebrew-aws-env-manager/archive/refs/tags/v1.0.3.tar.gz"
+    sha256 "111398df7976397e331f2fda4afb82f417bd9ae0232fb26c15cb9314c2b17d63"
     license "Apache-2.0"
     head "https://github.com/stanyzra/homebrew-aws-env-manager.git", branch: "main"
 
@@ -20,12 +20,21 @@ class EnvManager < Formula
         sha256 "342edb6f91d5839e790411822fc39f9c712c87cdaa7f3b1999f50b1ca16c4a14"
     end
          
+    resource "six" do
+    url "https://files.pythonhosted.org/packages/71/39/171f1c67cd00715f190ba0b100d606d440a28c93c7714febeca8b79af85e/six-1.16.0.tar.gz"
+    sha256 "1e61c37477a1626458e36f7b1d82aa5c9b094fa4802892072e49de9c60c4c926"
+  end
+
+  resource "parsedatetime" do
+    url "https://files.pythonhosted.org/packages/a8/20/cb587f6672dbe585d101f590c3871d16e7aec5a576a1694997a3777312ac/parsedatetime-2.6.tar.gz"
+    sha256 "4cb368fbb18a0b7231f4d76119165451c8d2e35951455dfee97c62a87b04d455"
+  end
+         
     def install
-        system "pip3", "install", "."
-        bin.install_symlink libexec/"bin/env_manager"
+        virtualenv_install_with_resources
     end
 
     test do
-    system bin/"env_manager", "--help"
+        system bin/"env_manager", "--help"
     end
 end
