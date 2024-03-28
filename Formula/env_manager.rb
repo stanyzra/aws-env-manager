9b414e7d19cbc2161d912a82984705d2c22dbe94
stanyzra <stanyhelberth@gmail.com>
2024-03-28 17:04:44 -0300

    fix: update formulae

--- Formula/env_manager.rb
+++ Formula/env_manager.rb
@@ -10,18 +10,21 @@ class EnvManager < Formula
 
     depends_on "python@3.10"
 
-    resource "boto3" do
-        url "https://files.pythonhosted.org/packages/bf/cd/8e6468c2f462ebcd6629b4b0ff4c114b0266a06bcb5cbc958ae1db6dcfff/boto3-1.34.72.tar.gz"
-        sha256 "cbfabd99c113bbb1708c2892e864b6dd739593b97a76fbb2e090a7d965b63b82"
-    end
+    # resource "boto3" do
+    #     url "https://files.pythonhosted.org/packages/bf/cd/8e6468c2f462ebcd6629b4b0ff4c114b0266a06bcb5cbc958ae1db6dcfff/boto3-1.34.72.tar.gz"
+    #     sha256 "cbfabd99c113bbb1708c2892e864b6dd739593b97a76fbb2e090a7d965b63b82"
+    # end
         
-    resource "botocore" do
-        url "https://files.pythonhosted.org/packages/46/09/53419321db64184c34a700bc0951fdae752f236d6d2bb6be3fa3586cfd47/botocore-1.34.72.tar.gz"
-        sha256 "342edb6f91d5839e790411822fc39f9c712c87cdaa7f3b1999f50b1ca16c4a14"
-    end
+    # resource "botocore" do
+    #     url "https://files.pythonhosted.org/packages/46/09/53419321db64184c34a700bc0951fdae752f236d6d2bb6be3fa3586cfd47/botocore-1.34.72.tar.gz"
+    #     sha256 "342edb6f91d5839e790411822fc39f9c712c87cdaa7f3b1999f50b1ca16c4a14"
+    # end
          
     def install
-        virtualenv_install_with_resources
+        venv = virtualenv_create(libexec, "python3")
+        venv.pip_install resources
+        venv.pip_install_and_link buildpath
+        bin.install_symlink libexec/"bin/env_manager"
     end
 
     test do
